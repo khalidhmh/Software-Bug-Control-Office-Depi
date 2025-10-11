7934f8f871270da1d45d29fc2c6d4dc7054cec64
sara <saraessam222@gmail.com>
2025-10-11 10:00:52 +0300

    Added option to switch between list and grid view in actors screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)// Compose Compiler // هيلت plugin (نفس الاسم الموجود في toml)
 }
 
 
