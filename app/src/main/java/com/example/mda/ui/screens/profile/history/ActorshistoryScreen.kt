61e07de8f858cee05aaa945c36207ecadf7f7d2c
omarAtallah77 <omaratallah077@gmail.com>
2025-11-25 19:43:36 +0200

    enchance ui in actors history and movies history

--- app/src/main/java/com/example/mda/ui/screens/profile/history/ActorshistoryScreen.kt
+++ app/src/main/java/com/example/mda/ui/screens/profile/history/ActorshistoryScreen.kt
@@ -6,14 +6,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -22,12 +26,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.example.mda.ui.navigation.TopBarState
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @Composable
 fun HistoryScreen(
@@ -63,40 +74,69 @@ fun HistoryScreen(
 
     if (history.isEmpty()) {
         emptyScreen(
-            "No viewed people yet movies yet",
+            "No viewed people yet",
             " Start exploring and your viewed people will appear here.",
         )
 
     } else {
         LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(12.dp),
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier.padding(8.dp)
         ) {
             items(history) { person ->
-                Row(
+
+                Card(
                     modifier = Modifier
                         .fillMaxWidth()
+                        .padding(vertical = 8.dp)
                         .clickable {
                             navController.navigate("ActorDetails/${person.id}")
-                        }
+                        },
+                    shape = RoundedCornerShape(16.dp),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
                 ) {
-                    AsyncImage(
-                        model = "https://image.tmdb.org/t/p/w200${person.profilePath}",
-                        contentDescription = null,
-                        modifier = Modifier.size(70.dp)
-                    )
 
-                    Spacer(modifier = Modifier.width(12.dp))
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(12.dp)
+                    ) {
 
-                    Column {
-                        Text(person.name ?: "Actor Name", fontWeight = FontWeight.Bold)
-                        Text(
-                            text = person.viewedAt.toString() ?: "",
-                            style = MaterialTheme.typography.bodySmall
+                        AsyncImage(
+                            model = "https://image.tmdb.org/t/p/w200${person.profilePath}",
+                            contentDescription = person.name,
+                            modifier = Modifier
+                                .size(100.dp)
+                                .clip(RoundedCornerShape(8.dp)),
+                            contentScale = ContentScale.Crop
                         )
+
+                        Spacer(modifier = Modifier.width(16.dp))
+
+                        Column(
+                            modifier = Modifier.align(Alignment.CenterVertically)
+                        ) {
+                            Text(
+                                text = person.name ?: "Actor Name",
+                                style = MaterialTheme.typography.titleMedium,
+                                fontWeight = FontWeight.Bold
+                            )
+
+                            Spacer(modifier = Modifier.height(4.dp))
+
+                            Text(
+                                text = "Viewed on: ${
+                                    SimpleDateFormat(
+                                        "dd MMM yyyy",
+                                        Locale.getDefault()
+                                    ).format(Date(person.viewedAt))}",
+                                style = MaterialTheme.typography.bodySmall,
+                                color = Color.Gray
+                            )
+                        }
                     }
                 }
             }
+
         }
     }
 }
