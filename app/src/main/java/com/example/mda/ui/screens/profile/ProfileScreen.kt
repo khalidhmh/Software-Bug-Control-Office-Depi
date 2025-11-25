edab75459a33df981f3ed6a384214d9f82b45720
omarAtallah77 <omaratallah077@gmail.com>
2025-11-25 23:50:58 +0200

    Modifiy :setting screen by adding Actors , moviess viewed and favourite movies to it

--- app/src/main/java/com/example/mda/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/mda/ui/screens/profile/ProfileScreen.kt
@@ -22,10 +22,7 @@ import coil.compose.AsyncImage
 import com.example.mda.ui.navigation.TopBarState
 import com.example.mda.ui.screens.auth.AuthViewModel
 import com.example.mda.ui.screens.favorites.FavoritesViewModel
-import com.example.mda.ui.screens.favorites.components.FavoriteButton
-import com.example.mda.ui.screens.home.homeScreen.MovieCardWithFavorite
 import com.example.mda.ui.theme.AppBackgroundGradient
-import com.example.mda.ui.screens.profile.favourites.HistorySectionButton
 
 @Composable
 fun ProfileScreen(
